4480b9b8b67894f2486ee38614503c3c2064d4fd
Carlos <dey@av.i>
2021-04-15 08:39:31 +0200

    Changes for new payload format

--- plm.sh
+++ plm.sh
@@ -5,9 +5,15 @@ config=$(cat data/config.json)
 server=$(echo "$config" | jq -r .mqtt_server)
 username=$(echo "$config" | jq -r .mqtt_user)
 password=$(echo "$config" | jq -r .mqtt_password)
+pixlet_server=$(echo "$config" | jq -r .pixlet_server)
 
 data="$2"
 topic="$1"
-[[ "$topic" == "applet" ]] && data=$(curl -s "http://192.168.1.10:8080/applet/$2") # https://github.com/cghdev/pixlet
-
-docker run --rm -it ruimarinho/mosquitto mosquitto_pub -h $server  -u "$username" -P "$password" -t "plm/$topic"  -m "$data"
+if [[ "$topic" == "applet" ]];then
+    data=$(curl -s "http://$pixlet_server:8080/applet/$2") # https://github.com/cghdev/pixlet
+    [[ "$data" == "" ]] && echo "Applet does not exist" && exit 1
+    payload="{\"applet\": \"$2\", \"payload\": \"$data\"}"
+else
+    payload=$data
+fi
+docker run --rm -it ruimarinho/mosquitto mosquitto_pub -h $server  -u "$username" -P "$password" -t "plm/$topic"  -m "$payload"
