e2366526598860cf6fee6dcf8f917c7a8ae905c4
Carlos <dey@av.i>
2021-04-21 00:00:51 +0200

    Check json keys before using them. Some formatting

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -25,60 +25,63 @@ WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  // Serial.printf("Message received [%s]\n", topic);
-  // Serial.printf("Heap available: %d\n", ESP.getFreeHeap());
+    // Serial.printf("Message received [%s]\n", topic);
+    // Serial.printf("Heap available: %d\n", ESP.getFreeHeap());
 
-  if (strcmp(topic, APPLET_TOPIC) == 0) {
-    payload[length] = '\0';
-    appletData = (char*)payload;
-    DeserializationError error = deserializeJson(doc, appletData, length);
+    if (strcmp(topic, APPLET_TOPIC) == 0) {
+        payload[length] = '\0';
+        appletData = (char*)payload;
+        DeserializationError error = deserializeJson(doc, appletData, length);
 
-    if (error) {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.f_str());
-      return;
+        if (error) {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.f_str());
+            return;
+        }
+        const char* applet_name;
+        const char* applet;
+        if (doc.containsKey("applet")) 
+            applet_name = doc["applet"];
+        if (doc.containsKey("payload"))
+            applet = doc["payload"];
+
+        free(base64decoded);
+        base64decoded = base64_decode((const unsigned char*)applet, strlen(applet), &outputLength);
+        if (base64decoded && outputLength > 1 && strncmp((const char*)base64decoded, "GIF8", 4) == 0) {
+            free(appletdecoded);
+            appletdecoded = (unsigned char *)malloc(outputLength);
+            memcpy(appletdecoded, base64decoded, outputLength);
+
+            newapplet = true;
+            currentMode = APPLET;
+            client.publish(CURRENT_APP, applet_name, true);
+        } else {
+            Serial.println(F("Error decoding base64. Not valid base64 or not GIF image"));
+        }
     }
-
-    const char* applet_name = doc["applet"];
-    const char* applet = doc["payload"];
-
-    free(base64decoded);
-    base64decoded = base64_decode((const unsigned char*)applet, strlen(applet), &outputLength);
-    if (base64decoded && outputLength > 1 && strncmp((const char*)base64decoded, "GIF8", 4) == 0) {
-      free(appletdecoded);
-      appletdecoded = (unsigned char *)malloc(outputLength);
-      memcpy(appletdecoded, base64decoded, outputLength);
-
-      newapplet = true;
-      currentMode = APPLET;
-      client.publish(CURRENT_APP, applet_name, true);
-    } else {
-      Serial.println(F("Error decoding base64. Not valid base64 or not GIF image"));
+    if (strcmp(topic, BRIGHTNESS_TOPIC) == 0) {
+        payload[length] = '\0';
+        brightness = atoi((char*)payload);
     }
-  }
-  if (strcmp(topic, BRIGHTNESS_TOPIC) == 0) {
-    payload[length] = '\0';
-    brightness = atoi((char*)payload);
-  }
 }
 
 
 void mqttReconnect(char* mqtt_user, char* mqtt_password) {
-  Serial.println(F("mqttReconnect"));
+    Serial.println(F("mqttReconnect"));
 
-  while (!client.connected()) {
-    Serial.println(F("Trying to reconnect to MQTT"));
-    if (client.connect(MQTT_CLIENT, mqtt_user, mqtt_password)) {
-      Serial.println(F("Connected to MQTT"));
-      client.publish(STATUS_TOPIC, (const char *)"up");
-      client.subscribe(STATUS_TOPIC);
-      client.subscribe(BRIGHTNESS_TOPIC);
-      client.subscribe(APPLET_TOPIC);
-    } else {
-      Serial.print(F("Failed, rc="));
-      Serial.print(client.state());
-      Serial.println(F("Retrying in 5 seconds"));
-      delay(5000);
+    while (!client.connected()) {
+        Serial.println(F("Trying to reconnect to MQTT"));
+        if (client.connect(MQTT_CLIENT, mqtt_user, mqtt_password)) {
+            Serial.println(F("Connected to MQTT"));
+            client.publish(STATUS_TOPIC, (const char *)"up");
+            client.subscribe(STATUS_TOPIC);
+            client.subscribe(BRIGHTNESS_TOPIC);
+            client.subscribe(APPLET_TOPIC);
+        } else {
+            Serial.print(F("Failed, rc="));
+            Serial.print(client.state());
+            Serial.println(F("Retrying in 5 seconds"));
+            delay(5000);
+        }
     }
-  }
 }
