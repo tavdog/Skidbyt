3de73fc3a9e80ff0184fd7eb39903263864e5695
Tavis <github@tavis.la>
2024-11-28 18:40:50 -1000

    new downloadGif function

--- src/main.cpp
+++ src/main.cpp
@@ -2,120 +2,141 @@
 #include "SPIFFS.h"
 
 #include <Arduino.h>
+#include <LCBUrl.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <WiFiManager.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h> 
-#include <time.h>
-#include <ArduinoOTA.h>
-
+#include <ArduinoJson.h>
 #include <constants.h>
-#include <mqtt.h>
-//#include <password.h>
-
 #include <Tidbyt_ESP32_RGB64x32MatrixPanel-I2S-DMA.h>
-
 #include <SmartMatrix.h>
-
 #include <GifDecoder.h>
 
+// lots of smart matrix setup happens in here
+#include "smart_matrix_setup.h"
 
-#define DISPLAY_TIME_SECONDS 10
-#define NUMBER_FULL_CYCLES   100
-
-#define USE_SMARTMATRIX         1
-#define ENABLE_SCROLLING        1
-
-
-void setupOTA();
-void setupTopics();
-
-#if (USE_SMARTMATRIX == 1)
-/* SmartMatrix configuration and memory allocation */
-#define COLOR_DEPTH 24                  // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
-const uint16_t kMatrixWidth = 64;       // Set to the width of your display, must be a multiple of 8
-const uint16_t kMatrixHeight = 32;      // Set to the height of your display
-const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
-const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
-const uint8_t kPanelType = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;  // Choose the configuration that matches your panels.  See more details in MatrixCommonHUB75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
-const uint32_t kMatrixOptions = (SM_HUB75_OPTIONS_NONE);        // see docs for options: https://github.com/pixelmatix/SmartMatrix/wiki
-const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
-const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
-
-SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
-SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
-#if (ENABLE_SCROLLING == 1)
-SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
-#endif
-#endif
-
-/* template parameters are maxGifWidth, maxGifHeight, lzwMaxBits
- * 
- * lzwMaxBits is included for backwards compatibility reasons, but isn't used anymore
- */
-GifDecoder<kMatrixWidth, kMatrixHeight, 12> decoder;
-// range 0-255
-int defaultBrightness = (10*255) / 100;
-
-const rgb24 COLOR_BLACK = {
-    0, 0, 0 };
-void screenClearCallback(void) {
-#if (USE_SMARTMATRIX == 1)
-    backgroundLayer.fillScreen({0,0,0});
-#endif
-}
-
-void updateScreenCallback(void) {
-#if (USE_SMARTMATRIX == 1)
-    backgroundLayer.swapBuffers();
-#endif
-}
-
-void drawPixelCallback(int16_t x, int16_t y, uint8_t red, uint8_t green, uint8_t blue) {
-#if (USE_SMARTMATRIX == 1)
-    backgroundLayer.drawPixel(x, y, {red, green, blue});
-#endif
-}
-#define configTIMER_TASK_STACK_DEPTH 4096
-
-int defaultScrollOffset = 6;
-
-// Variables de configuración para el AP WiFi
-// char host[] = "PLM";
-// char separator[] = "_";
-// char SSID[10];
+// AP WiFi
+char host[] = "Skidbyt";
+char separator[] = "_";
+char SSID[10];
 byte mac[6];
 char macFull[6];
 
-void configModeCallback (WiFiManager *myWiFiManager) {
-    
+
+
+uint8_t *downloadGIF(char* curl, size_t &fileSize)
+{
+    LCBUrl url;
+    url.setUrl(curl); 
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        // Function to download the GIF file
+            WiFiClientSecure client;
+            client.setInsecure(); // Disable SSL certificate verification (use only if your server does not have a valid certificate)
+
+            Serial.println("Connecting to server...");
+            if (!client.connect(url.getHost().c_str(), url.getPort()))
+            {
+                Serial.println("Connection to server failed!");
+                return nullptr;
+            }
+
+            // Construct the HTTP GET request
+            String request = String("GET ") + url.getPath() + " HTTP/1.1\r\n" +
+                             "Host: " + url.getHost() + "\r\n" +
+                             "Connection: close\r\n\r\n";
+
+            // Send the request
+            client.print(request);
+
+            // Wait for the response
+            while (client.connected() && !client.available())
+            {
+                delay(100);
+            }
+
+            // Parse HTTP response
+            if (!client.find("Content-Length:"))
+            {
+                Serial.println("Failed to find Content-Length header!");
+                return nullptr;
+            }
+
+            fileSize = client.parseInt(); // Get the file size (we already did a find "Content-Length" above)
+            Serial.printf("Content-Length: %d bytes\n", fileSize);
+
+            // Skip HTTP headers
+            if (!client.find("\r\n\r\n"))
+            {
+                Serial.println("Invalid HTTP headers!");
+                return nullptr;
+            }
+
+            // Allocate memory for the GIF file
+            uint8_t *gifBuffer = (uint8_t *)malloc(fileSize);
+            if (!gifBuffer)
+            {
+                Serial.println("Failed to allocate memory for GIF!");
+                return nullptr;
+            }
+
+            // Read the GIF file into the buffer
+            size_t bytesRead = 0;
+            while (bytesRead < fileSize)
+            {
+                size_t chunkSize = client.readBytes(gifBuffer + bytesRead, fileSize - bytesRead);
+                if (chunkSize == 0)
+                {
+                    Serial.println("Failed to read data!");
+                    free(gifBuffer);
+                    return nullptr;
+                }
+                bytesRead += chunkSize;
+            }
+
+            Serial.println("GIF file downloaded successfully!");
+            return gifBuffer;
+    }
+    else
+    {
+        Serial.println("WiFi disconnected");
+        delay(2000);
+    }
+}
+
+void configModeCallback(WiFiManager *myWiFiManager)
+{
+
     scrollingLayer.stop();
     scrollingLayer.setColor(WHITE);
     scrollingLayer.setMode(wrapForward);
     scrollingLayer.setSpeed(80);
     scrollingLayer.setFont(font6x10);
-    scrollingLayer.start("Unable to connect. Access Point:", -1);
+    scrollingLayer.start("Connect to Access Point:", -1);
 
     // Show SSID
     backgroundLayer.setFont(font5x7);
-    backgroundLayer.drawString(0, 21, LIGHT_BLUE, hostName);
+    backgroundLayer.drawString(0, 21, LIGHT_BLUE, host);
     backgroundLayer.swapBuffers();
 }
 
 StaticJsonDocument<3500> document;
 
-char mqtt_server[40];
-char mqtt_port[8];
-char mqtt_user[40];
-char mqtt_password[40];
+char url1[100];
+char url2[100];
+char url3[100];
+char url4[100];
 
 bool saveConfig = false;
 
-void saveConfigCallback () {
+void saveConfigCallback()
+{
     saveConfig = true;
     Serial.println(F("We need to save the config"));
 }
 
-void setup() {
+void setup()
+{
     decoder.setScreenClearCallback(screenClearCallback);
     decoder.setUpdateScreenCallback(updateScreenCallback);
     decoder.setDrawPixelCallback(drawPixelCallback);
@@ -125,60 +146,67 @@ void setup() {
 
     Serial.println(F("Mounting FS..."));
 
-    if (SPIFFS.begin(true)) {
-        if (SPIFFS.exists("/config.json")) {
+    if (SPIFFS.begin(true))
+    {
+        if (SPIFFS.exists("/config.json"))
+        {
             Serial.println(F("FS mounted"));
             File configFile = SPIFFS.open("/config.json", "r");
-            if (configFile) {
+            if (configFile)
+            {
                 size_t size = configFile.size();
                 std::unique_ptr<char[]> buf(new char[size]);
 
                 configFile.readBytes(buf.get(), size);
                 DynamicJsonDocument doc(256);
                 DeserializationError error = deserializeJson(doc, buf.get());
-                if (error) {
+                if (error)
+                {
                     Serial.println(F("There was an error reading the config.json file"));
                     return;
                 }
                 JsonObject json = doc.as<JsonObject>();
 
                 serializeJson(json, Serial);
-                if (!json.isNull()) {
+                if (!json.isNull())
+                {
 
-                    strcpy(mqtt_server, json["mqtt_server"]);
-                    strcpy(mqtt_port, json["mqtt_port"]);
-                    strcpy(mqtt_user, json["mqtt_user"]);
-                    strcpy(mqtt_password, json["mqtt_password"]);
-
-                } else {
+                    strcpy(url1, json["url1"]);
+                    strcpy(url2, json["url2"]);
+                    strcpy(url3, json["url3"]);
+                    strcpy(url4, json["url4"]);
+                }
+                else
+                {
                     Serial.println(F("Something happened"));
                 }
             }
         }
-    } else {
+    }
+    else
+    {
         // Serial.println("Couldn't mount the FS");
-    } 
+    }
 
-    WiFiManagerParameter custom_mqtt_server("server", "MQTT server", mqtt_server, 40);
-    WiFiManagerParameter custom_mqtt_port("port", "MQTT port", mqtt_port, 8);
-    WiFiManagerParameter custom_mqtt_user("user", "MQTT user", mqtt_user, 40);
-    WiFiManagerParameter custom_mqtt_pass("pass", "MQTT password", mqtt_password, 40);
+    WiFiManagerParameter custom_url1("server", "GIF URL 1", url1, 100);
+    WiFiManagerParameter custom_url2("url", "GIF URL 2", url2, 100);
+    WiFiManagerParameter custom_url3("url", "GIF URL 3", url3, 100);
+    WiFiManagerParameter custom_url4("url", "GIF URL 4", url4, 100);
 
     WiFiManager wifiManager;
     WiFi.macAddress(mac);
     uint8_t baseMac[6];
     esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
     sprintf(macFull, "%02X%02X%02X", baseMac[3], baseMac[4], baseMac[5]);
-    snprintf(hostName, 11, PSTR("PLM-%s"),macFull);
+    // snprintf(hostName, 11, PSTR("PLM-%s"), macFull);
 
-    matrix.addLayer(&backgroundLayer); 
-    matrix.addLayer(&scrollingLayer); 
+    matrix.addLayer(&backgroundLayer);
+    matrix.addLayer(&scrollingLayer);
     // matrix.addLayer(&indexedLayer);
     matrix.begin(28000);
     matrix.setBrightness(defaultBrightness);
     scrollingLayer.setOffsetFromTop(defaultScrollOffset);
     backgroundLayer.enableColorCorrection(true);
-
 
     scrollingLayer.setColor(WHITE);
     scrollingLayer.setMode(wrapForward);
@@ -187,7 +215,7 @@ void setup() {
     scrollingLayer.start("Connecting to WiFi...", -1);
 
     Serial.println(F("Connecting to WiFi..."));
-    WiFi.setHostname(hostName);
+    WiFi.setHostname(host);
     WiFi.setAutoReconnect(true);
 
     wifiManager.setAPCallback(configModeCallback);
@@ -195,163 +223,94 @@ void setup() {
     wifiManager.setCleanConnect(true);
     wifiManager.setSaveConfigCallback(saveConfigCallback);
 
-    wifiManager.addParameter(&custom_mqtt_server);
-    wifiManager.addParameter(&custom_mqtt_port);
-    wifiManager.addParameter(&custom_mqtt_user);
-    wifiManager.addParameter(&custom_mqtt_pass);
+    wifiManager.addParameter(&custom_url1);
+    wifiManager.addParameter(&custom_url2);
+    wifiManager.addParameter(&custom_url3);
+    wifiManager.addParameter(&custom_url4);
 
-    if (!wifiManager.autoConnect(hostName, "password")) {
+    if (!wifiManager.autoConnect(host, "password"))
+    {
         delay(3000);
     }
     scrollingLayer.stop();
     scrollingLayer.setMode(stopped);
-    scrollingLayer.start(hostName, -1);
+    scrollingLayer.start(host, -1);
     delay(3000);
 
     Serial.print(F("Hostname: "));
-    Serial.println(hostName);
+    Serial.println(host);
 
-    setupOTA();
-    setupTopics();
 
-    client.setServer(mqtt_server, atoi(mqtt_port));
-    client.connect(hostName, mqtt_user, mqtt_password);
-    client.setBufferSize(30000);
-    client.setCallback(mqttCallback);
-
-    if (client.connected()) {
-        Serial.println(F("Connected to MQTT (main loop)"));
-        client.publish(status_topic, (const char *)"up");
-        client.subscribe(status_topic);
-        client.subscribe(applet_topic);
-        client.subscribe(brightness_topic);
-
-    }
-
-    if (saveConfig) {
-        strcpy(mqtt_server, custom_mqtt_server.getValue());
-        strcpy(mqtt_port, custom_mqtt_port.getValue());
-        strcpy(mqtt_user, custom_mqtt_user.getValue());
-        strcpy(mqtt_password, custom_mqtt_pass.getValue());
+    if (saveConfig)
+    {
+        strcpy(url1, custom_url1.getValue());
+        strcpy(url2, custom_url2.getValue());
+        strcpy(url3, custom_url3.getValue());
+        strcpy(url4, custom_url4.getValue());
 
         DynamicJsonDocument doc(256);
         JsonObject json = doc.to<JsonObject>();
 
-        json["mqtt_server"] = mqtt_server;
-        json["mqtt_port"]   = mqtt_port;
-        json["mqtt_user"]   = mqtt_user;
-        json["mqtt_password"]   = mqtt_password;
+        json["url1"] = url1;
+        json["url2"] = url2;
+        json["url3"] = url3;
+        json["url4"] = url4;
 
         File configFile = SPIFFS.open("/config.json", "w");
-        if (!configFile) {
-        Serial.println(F("There was an error opening the file to write"));
+        if (!configFile)
+        {
+            Serial.println(F("There was an error opening the file to write"));
         }
 
         serializeJson(json, configFile);
         configFile.close();
         saveConfig = false;
     }
-
 }
 
-void loop() {
+uint32_t currentFrameDelay = 5000; // ms
+
+void loop()
+{
     // Try to reconnect to wifi if connection lost
-    while (WiFi.status() != WL_CONNECTED || WiFi.localIP() == IPAddress(0,0,0,0)) {
+    while (WiFi.status() != WL_CONNECTED || WiFi.localIP() == IPAddress(0, 0, 0, 0))
+    {
         WiFi.reconnect();
         Serial.println(F("Trying to reconnect to WiFi"));
-        delay(10000);
+        delay(3000);
     }
 
-    client.loop();
-    ArduinoOTA.handle();
+    // if (brightness > 0)
+    // {
+    //     matrix.setBrightness((brightness * 255) / 100);
+    //     brightness = -1;
+    // }
+    // if (currentMode == WELCOME)
+    // {
+    //     scrollingLayer.setColor(WHITE);
+    //     scrollingLayer.setMode(wrapForward);
+    //     scrollingLayer.setSpeed(60);
+    //     scrollingLayer.setFont(font6x10);
+    //     scrollingLayer.start("Getting images...", -1);
+    //     currentMode = APPLET;
+    // }
+    // if (currentMode == APPLET)
+    // {
+    //     scrollingLayer.stop();
+    //     static uint32_t lastFrameDisplayTime = 0;
+    //     unsigned long now = millis();
 
-    if (!client.connected()) {
-        Serial.println(F("Not connected to MQTT. Trying to reconnect."));
-        mqttReconnect(mqtt_user, mqtt_password);
-    }
+    //     if ((millis() - lastFrameDisplayTime) > currentFrameDelay)
+    //     {
+    //         if (newapplet)
+    //         {
+    //             decoder.startDecoding((uint8_t *)appletdecoded, outputLength);
+    //             newapplet = false;
+    //         }
+    //         decoder.decodeFrame(false);
 
-    if (brightness > 0) {
-        matrix.setBrightness((brightness*255) / 100);
-        brightness = -1;
-    }
-    if (currentMode == WELCOME) {
-        scrollingLayer.setColor(WHITE);
-        scrollingLayer.setMode(wrapForward);
-        scrollingLayer.setSpeed(60);
-        scrollingLayer.setFont(font6x10);
-        scrollingLayer.start("Waiting for applets...", -1);
-        currentMode = NONE;
-    }
-    if (currentMode == APPLET) {
-        scrollingLayer.stop();
-        static uint32_t lastFrameDisplayTime = 0;
-        static unsigned int currentFrameDelay = 0;
-
-        unsigned long now = millis();
-
-        if((millis() - lastFrameDisplayTime) > currentFrameDelay) {
-            if (newapplet) {
-                decoder.startDecoding((uint8_t *)appletdecoded, outputLength);
-                newapplet = false;
-            }
-            decoder.decodeFrame(false);
-
-            lastFrameDisplayTime = now;
-            currentFrameDelay = decoder.getFrameDelay_ms();
-        }
-    }
-}
-
-void setupTopics() {
-    snprintf_P(status_topic, 18, PSTR("%s/%s/status"), TOPIC_PREFIX, macFull);
-    snprintf_P(current_app, 19, PSTR("%s/%s/current"), TOPIC_PREFIX, macFull);
-    snprintf_P(applet_topic, 18, PSTR("%s/%s/applet"), TOPIC_PREFIX, macFull);
-    Serial.println(applet_topic);
-    snprintf_P(brightness_topic, 22, PSTR("%s/%s/brightness"), TOPIC_PREFIX, macFull);
-    snprintf_P(heap_topic, 20, PSTR("%s/%s/freeheap"), TOPIC_PREFIX, macFull);
-}
-
-void setupOTA() {
-
-    // Port defaults to 3232
-    // ArduinoOTA.setPort(3232);
-
-    // Hostname defaults to esp3232-[MAC]
-    ArduinoOTA.setHostname(hostName);
-
-    // No authentication by default
-    // ArduinoOTA.setPassword(otaPassword);
-
-    // Password can be set with it's md5 value as well
-    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-    ArduinoOTA
-        .onStart([]() {
-        scrollingLayer.stop();
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH)
-            type = "sketch";
-        else // U_SPIFFS
-            type = "filesystem";
-
-        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-        // Serial.println("Start updating " + type);
-        })
-        .onEnd([]() {
-            // Serial.println("\nEnd");
-        })
-        .onProgress([](unsigned int progress, unsigned int total) {
-            // Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-        })
-        .onError([](ota_error_t error) {
-            // Serial.printf("Error[%u]: ", error);
-            // if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-        //     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-        //     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-        //     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-        //     else if (error == OTA_END_ERROR) Serial.println("End Failed");
-        });
-
-    ArduinoOTA.begin();
+    //         lastFrameDisplayTime = now;
+    //         currentFrameDelay = decoder.getFrameDelay_ms();
+    //     }
+    // }
 }
