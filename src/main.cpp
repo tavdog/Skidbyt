33ce359edb3899495e1dd0a7e3cfd2bed72a8a56
Tavis <github@tavis.la>
2024-12-06 10:41:50 -1000

    move smart matrix code setup file

--- src/main.cpp
+++ src/main.cpp
@@ -29,55 +29,7 @@
 void setupOTA();
 void setupTopics();
 
-#if (USE_SMARTMATRIX == 1)
-/* SmartMatrix configuration and memory allocation */
-#define COLOR_DEPTH 24                  // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
-const uint16_t kMatrixWidth = 64;       // Set to the width of your display, must be a multiple of 8
-const uint16_t kMatrixHeight = 32;      // Set to the height of your display
-const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
-const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
-const uint8_t kPanelType = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;  // Choose the configuration that matches your panels.  See more details in MatrixCommonHUB75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
-const uint32_t kMatrixOptions = (SM_HUB75_OPTIONS_NONE);        // see docs for options: https://github.com/pixelmatix/SmartMatrix/wiki
-const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
-const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
-
-SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
-SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
-#if (ENABLE_SCROLLING == 1)
-SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
-#endif
-#endif
-
-/* template parameters are maxGifWidth, maxGifHeight, lzwMaxBits
- * 
- * lzwMaxBits is included for backwards compatibility reasons, but isn't used anymore
- */
-GifDecoder<kMatrixWidth, kMatrixHeight, 12> decoder;
-// range 0-255
-int defaultBrightness = (10*255) / 100;
-
-const rgb24 COLOR_BLACK = {
-    0, 0, 0 };
-void screenClearCallback(void) {
-#if (USE_SMARTMATRIX == 1)
-    backgroundLayer.fillScreen({0,0,0});
-#endif
-}
-
-void updateScreenCallback(void) {
-#if (USE_SMARTMATRIX == 1)
-    backgroundLayer.swapBuffers();
-#endif
-}
-
-void drawPixelCallback(int16_t x, int16_t y, uint8_t red, uint8_t green, uint8_t blue) {
-#if (USE_SMARTMATRIX == 1)
-    backgroundLayer.drawPixel(x, y, {red, green, blue});
-#endif
-}
-#define configTIMER_TASK_STACK_DEPTH 4096
-
-int defaultScrollOffset = 6;
+#include "smart_matrix_setup.h"
 
 // Variables de configuración para el AP WiFi
 // char host[] = "PLM";
